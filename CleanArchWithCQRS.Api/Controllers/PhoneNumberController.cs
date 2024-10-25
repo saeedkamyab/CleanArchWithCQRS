e69fe40d7adf1281dded602071dfeec518d30bb8
saeedkamyab <saeed.kamyab@yahoo.com>
2024-10-25 21:26:31 +0330

    This is ready For Using

---
+++ CleanArchWithCQRS.Api/Controllers/PhoneNumberController.cs
@@ -0,0 +1,32 @@
+﻿using CleanArchWithCQRS.Application.Dtos.PhoneNumberDtos;
+using CleanArchWithCQRS.Application.Features.PhoneNumber.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchWithCQRS.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhoneNumberController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PhoneNumberController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<PhoneNumberDto>>> GetAll()
+        {
+            var phoneNumbers = await _mediator.Send(new GetPhoneNumbersRequest());
+            return phoneNumbers;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<PhoneNumberDto>>> GetStudentPhoneNumbers(Guid id)
+        {
+            var phoneNumbers = await _mediator.Send(new GetStudentPhoneNumbersRequest { StudentId = id });
+            return phoneNumbers;
+        }
+    }
+}
